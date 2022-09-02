6486feae330bde76c029c85bc32fdc0705f78607
Sanika03 <sanikasuryawanshi0305@gmail.com>
2022-09-02 15:31:14 +0530

    input processing output done

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-var btnTranslate = document.querySelector("#btn-translator");
+var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-
-console.log(txtInput);
+var outputDiv = document.querySelector("#output");
 
 function clickHandler() {
-    console.log("clicked!");
-    console.log(input, txtInput.value);
+    outputDiv.innerText = "banana nana baba banana" + txtInput.value;
 }
 
 btnTranslate.addEventListener("click", clickHandler);
