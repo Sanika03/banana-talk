ab8e794a6e301bff86b1a7aaff2bd00af74bc11d
Sanika03 <sanikasuryawanshi0305@gmail.com>
2022-09-03 02:24:24 +0530

    constructed url

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function getUrl(input){
+    return (serverUrl + "?" + "text=" + input);
+}
+
 function clickHandler() {
     outputDiv.innerText = "banana nana baba banana" + txtInput.value;
 }
